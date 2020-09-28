0fab5d66c87480eea4a0b1741748825ae82785a5
masummodi <masum.modi@yobored.com>
2020-09-28 02:54:24 -0400

    Initial Commit

---
+++ Hello World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by Masum Modi on 2020-09-28.
+//  Copyright © 2020 Centennial College. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
